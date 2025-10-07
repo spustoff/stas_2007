64a808ba19d3e13f777755cd661cb22ccd3bffc5
spustoff <vyacheslavpustov05@gmail.com>
2025-10-07 21:13:51 +0300

    1.1

--- stas_2007/ContentView.swift
+++ stas_2007/ContentView.swift
@@ -50,7 +50,7 @@ struct ContentView: View {
     
     private func check_data() {
         
-        let lastDate = "13.10.2025"
+        let lastDate = "14.10.2025"
         
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd.MM.yyyy"
